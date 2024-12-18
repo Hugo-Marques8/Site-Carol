a069be3d71566238335c915e3d6bb65323db1d3e
Hugo <hugomarques888@outlook.com>
2024-12-18 20:37:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Seleciona os elementos
-const buguer = document.getElementById('buguer');
-const menu = document.getElementById('menu');
-
-// Função para alternar a visibilidade do menu
-buguer.addEventListener('click', function() {
-  // Verifica se o menu está visível
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none'; // Esconde o menu
-  } else {
-    menu.style.display = 'block'; // Exibe o menu
-  }
+document.getElementById("buguer").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 });
