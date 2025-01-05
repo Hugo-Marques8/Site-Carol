2b72e5a7a0bb0d4cfe5bcc94a0d7827114246d0d
Hugo <hugomarques888@outlook.com>
2025-01-05 13:03:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.getElementById("buguer").addEventListener("click", function () {
   const menu = document.getElementById("menu");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('div');
+  sections.forEach(section => {
+      section.addEventListener('scroll', (e) => {
+          e.currentTarget.scrollIntoView({ behavior: 'smooth' });
+      });
+  });
+});
